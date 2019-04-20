625f34e85d0bdd54f1d75f365a5696719fc907d9
dzapfel <dzapfel@msudenver.edu>
2019-04-20 10:19:50 -0600

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+house_members_by_session = {
+    'one': pd.read_csv('data/house-101st.csv'),
+    'two': pd.read_csv('data/house-102nd.csv'),
+    'three': pd.read_csv('data/house-103rd.csv'),
+    'four': pd.read_csv('data/house-104th.csv'),
+    'five': pd.read_csv('data/house-105th.csv'),
+    'six': pd.read_csv('data/house-106th.csv'),
+    'seven': pd.read_csv('data/house-107th.csv'),
+    'eight': pd.read_csv('data/house-108th.csv'),
+    'nine': pd.read_csv('data/house-109th.csv'),
+    'ten': pd.read_csv('data/house-110th.csv'),
+    'eleven': pd.read_csv('data/house-111th.csv'),
+    'twelve': pd.read_csv('data/house-112th.csv'),
+    'thirteen': pd.read_csv('data/house-113th.csv'),
+    'fourteen': pd.read_csv('data/house-114th.csv'), 
+    'fifteen': pd.read_csv('data/house-115th.csv'),
+    'sixteen': pd.read_csv('data/house-116th.csv')
+}
+
+senate_members_by_session = {
+    'one': pd.read_csv('data/senate-101st.csv'),
+    'two': pd.read_csv('data/senate-102nd.csv'),
+    'three': pd.read_csv('data/senate-103rd.csv'),
+    'four': pd.read_csv('data/senate-104th.csv'),
+    'five': pd.read_csv('data/senate-105th.csv'),
+    'six': pd.read_csv('data/senate-106th.csv'),
+    'seven': pd.read_csv('data/senate-107th.csv'),
+    'eight': pd.read_csv('data/senate-108th.csv'),
+    'nine': pd.read_csv('data/senate-109th.csv'),
+    'ten': pd.read_csv('data/senate-110th.csv'),
+    'eleven': pd.read_csv('data/senate-111th.csv'),
+    'twelve': pd.read_csv('data/senate-112th.csv'),
+    'thirteen': pd.read_csv('data/senate-113th.csv'),
+    'fourteen': pd.read_csv('data/senate-114th.csv'), 
+    'fifteen': pd.read_csv('data/senate-115th.csv'),
+    'sixteen': pd.read_csv('data/senate-116th.csv')
+}
